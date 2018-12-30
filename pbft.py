c8426d6d630242f125d8b5570f66f78b80daa19e
ayushiagg <43211450+ayushiagg@users.noreply.github.com>
2018-12-30 21:49:42 +0530

    states and number of faulty, total nodes

--- pbft.py
+++ pbft.py
@@ -1,6 +1,18 @@
+from math import floor
+
+PBFT_TOTAL_NODES = 10
+PBFT_F = floor((PBFT_TOTAL_NODES - 1) / 3)
+PBFT_STATES = {"NONE": 0, "PRE-PREPARE": 1, "PREPARE": 2, "COMMIT": 3}
+
+
 class PBFT:
     """
-        Pbft class for implementing PBFT protocol, for consensus within a committee.
+        Pbft class for implementing PBFT protocol, for consensus
+        within a committee.
     """
-    pass
 
+    def __init__(self):
+        """
+        """
+
+    pass
