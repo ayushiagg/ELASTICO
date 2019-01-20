ec02d5a82f19cf6afee27c58b4b82ead4f88a226
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-01-20 17:08:49 +0530

    comments

--- tcptest/client.py
+++ tcptest/client.py
@@ -1,5 +1,5 @@
 # Import socket module 
-import socket			 
+import socket
 import json
 
 def sendMsg(socketconn, data):
@@ -11,10 +11,10 @@ def sendMsg(socketconn, data):
 
 
 # Create a socket object 
-s = socket.socket()		 
+s = socket.socket()
 
 # Define the port on which you want to connect 
-port = 12346		
+port = 12346
 
 # connect to the server on local computer 
 s.connect(('127.0.0.1', port)) 
@@ -25,4 +25,4 @@ print( s.recv(1024) )
 msg = {"type" : "msgtype", "data" : "msgdata"*1000}
 s.send(json.dumps(msg).encode())
 # close the connection 
-s.close()	 
+s.close()
