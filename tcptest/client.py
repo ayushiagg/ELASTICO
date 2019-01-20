ede61b7477fa301b9b26dfd545d61a925ed6ac51
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-01-20 17:46:00 +0530

    try, except block if connection error

--- tcptest/client.py
+++ tcptest/client.py
@@ -17,12 +17,13 @@ s = socket.socket()
 port = 12346
 
 # connect to the server on local computer 
-s.connect(('127.0.0.1', port)) 
-
-# receive data from the server 
-print( s.recv(1024) )
-
-msg = {"type" : "msgtype", "data" : "msgdata"*1000}
-s.send(json.dumps(msg).encode())
-# close the connection 
-s.close()
+try:
+	s.connect(('127.0.0.1', port))
+	# receive data from the server
+	print( s.recv(1024) )
+	msg = {"type" : "msgtype", "data" : "msgdata"*1000}
+	s.send(json.dumps(msg).encode())
+	# close the connection
+	s.close()
+except Exception as e:
+	print("Connection not possible")
