5b0b6e8f5a1a53e99a1f0fe59ddb4518b98ccd9a
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-01-20 17:05:55 +0530

    some test file for trying out tcp send recv

---
+++ tcptest/client.py
@@ -0,0 +1,28 @@
+# Import socket module 
+import socket			 
+import json
+
+def sendMsg(socketconn, data):
+	"""
+	"""
+	serialized_data = json.dumps(data)
+	encoded_data = serialized_data.encode()
+	socketconn.send(encoded_data)
+
+
+# Create a socket object 
+s = socket.socket()		 
+
+# Define the port on which you want to connect 
+port = 12346		
+
+# connect to the server on local computer 
+s.connect(('127.0.0.1', port)) 
+
+# receive data from the server 
+print( s.recv(1024) )
+
+msg = {"type" : "msgtype", "data" : "msgdata"*1000}
+s.send(json.dumps(msg).encode())
+# close the connection 
+s.close()	 
