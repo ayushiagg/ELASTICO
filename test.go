0bff1e494ce6c4b0718db073316634daa17c9381
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-02-08 16:47:35 +0530

    test file added

---
+++ test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"crypto/rsa"
+	"crypto/rand"
+	"reflect"
+
+)
+type Elastico struct{
+	key *rsa.PrivateKey
+	IP string
+}
+func main() {
+	e:= Elastico{}
+	var err error 
+	e.key,err = rsa.GenerateKey(rand.Reader, 2048)
+	if err!= nil{
+		fmt.Println(e.key)
+	}
+	fmt.Println(reflect.TypeOf(e.key))
+
+	c := 4
+	i := make([]byte, c)
+	_, err = rand.Read(i)
+	if err != nil {
+		fmt.Println("error:", err.Error)
+	}
+	e.IP= fmt.Sprintf("%v.%v.%v.%v" , i[0] , i[1], i[2], i[3])
+	fmt.Println(e.IP)
+}
