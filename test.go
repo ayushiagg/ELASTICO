cc667a02b9356a5b479f7614c938746ff773de5f
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-02-10 16:19:15 +0530

    generate random integer

--- test.go
+++ test.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rsa"
 	"crypto/rand"
 	"reflect"
+	"math/big"
 
 )
 type Elastico struct{
@@ -28,4 +29,16 @@ func main() {
 	}
 	e.IP= fmt.Sprintf("%v.%v.%v.%v" , i[0] , i[1], i[2], i[3])
 	fmt.Println(e.IP)
+	var v *big.Int
+	v.SetUint64(8)
+
+	var prime1, _ = new(big.Int).SetString("256", 10)
+	// Generate random numbers in range [0..prime1]
+	// Ignore error values
+	// Don't use this code to generate secret keys that protect important stuff!
+	x, _ := rand.Int(rand.Reader, prime1)
+	y, _ := rand.Int(rand.Reader, prime1)
+	fmt.Printf("x: %v\n", x)
+	fmt.Printf("y: %v\n", y)
+
 }
