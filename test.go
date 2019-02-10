9fa8df40b3b35b152c69dd827d1a7f6797093478
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-02-10 18:32:17 +0530

    test file updated

--- test.go
+++ test.go
@@ -6,6 +6,7 @@ import (
 	"crypto/rand"
 	"reflect"
 	"math/big"
+	"strconv"
 
 )
 type Elastico struct{
@@ -29,16 +30,14 @@ func main() {
 	}
 	e.IP= fmt.Sprintf("%v.%v.%v.%v" , i[0] , i[1], i[2], i[3])
 	fmt.Println(e.IP)
-	var v *big.Int
-	v.SetUint64(8)
-
-	var prime1, _ = new(big.Int).SetString("256", 10)
+	n:= 2345
+	str:= strconv.Itoa(n)
+	var prime1, _ = new(big.Int).SetString(str, 10)
 	// Generate random numbers in range [0..prime1]
 	// Ignore error values
-	// Don't use this code to generate secret keys that protect important stuff!
 	x, _ := rand.Int(rand.Reader, prime1)
-	y, _ := rand.Int(rand.Reader, prime1)
+	
 	fmt.Printf("x: %v\n", x)
-	fmt.Printf("y: %v\n", y)
+	
 
 }
