73cd57c266bf050af0bbe4d52a8ff157a553c146
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-02-18 11:09:56 +0530

    broadcast to ntw func defined

--- elastico.go
+++ elastico.go
@@ -52,6 +52,56 @@ func failOnError(err error, msg string) {
 }
 
 
+func BroadcastTo_Network(data map[string]interface{}, type_ string){
+	/*
+		Broadcast data to the whole ntw
+	*/
+	// construct msg
+	msg:= make(map[string]interface{})
+	msg["data"] = data
+	msg["type"] = type_	
+	
+	// establish a connection with RabbitMQ server
+	connection , err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")	// report the error
+	defer connection.Close()	// close the connection
+
+	ch, err := connection.Channel()	// create a channel
+	failOnError(err, "Failed to open a channel")		// report the error
+	defer ch.Close()	// close the channel
+ 
+	for _, node :=  range network_nodes{
+		
+		port := strconv.Itoa(node.port)
+
+		//create a hello queue to which the message will be delivered
+		queue, err := ch.QueueDeclare(
+			"hello" + port,	//name of the queue
+			false,	// durable
+			false,	// delete when unused
+			false,	// exclusive
+			false,	// no-wait
+			nil,	// arguments
+		)
+		failOnError(err, "Failed to declare a queue")
+
+		body, err := json.Marshal(msg)
+		failOnError(err, "Failed to marshal")
+		err = ch.Publish(
+			"",				// exchange
+			queue.Name,		// routing key
+			false,			// mandatory
+			false,			// immediate
+			amqp.Publishing {
+			ContentType: "text/plain",
+			Body:		body,
+		})
+
+		failOnError(err, "Failed to publish a message")
+	}
+}
+
+
 func random_gen(r int64) (*big.Int) {
 	/*
 		generate a random integer
