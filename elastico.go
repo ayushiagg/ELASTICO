8e5c023a0941b21c05c3d6e8a571b46bfcb6cebd
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-02-19 18:42:11 +0530

    sign the digest

--- elastico.go
+++ elastico.go
@@ -1167,8 +1167,7 @@ func sendPrepare(prepareMsgList []map[string]interface{}) {
 
 //Sign :- sign the byte array
 func (e *Elastico) Sign(digest []byte) string {
-
-	signed, err := rsa.SignPKCS1v15(rand.Reader, e.key, crypto.SHA256, digest) // sign the digest of Txn List
+	signed, err := rsa.SignPKCS1v15(rand.Reader, e.key, crypto.SHA256, digest) // sign the digest
 	failOnError(err, "Error in Signing byte array")
 	signature := base64.StdEncoding.EncodeToString(signed) // encode to base64
 	return signature
